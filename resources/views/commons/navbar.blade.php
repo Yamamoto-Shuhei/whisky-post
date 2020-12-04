b35775b539a11c3bb0cc10f9f76324dfdd97ac1b
yamamoto.shuhei <yamahei19981010@gmail.com>
2020-12-04 19:12:34 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,10 +11,24 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="nav navbar-nav navbar-right">
-                <li>{!! link_to_route('signup.get','ユーザー登録',[],['class'=>'nav-link']) !!}</li>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-                
+                @if (Auth::check())
+                    <li class="nav-item"></li>
+                    <li class="nav-item">
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            
+                            <li class="dropdown-item">プロフィール</li>
+                            <li class="dropdown-divider"></li>
+                            
+                            <li class="dropdown-item">{!! link_to_route('logout.get','ログアウト') !!}</li>
+                            
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">{!! link_to_route('signup.get','ユーザー登録',[],['class'=>'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login','ログイン',[],['class'=>'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
 </header>
+                        
